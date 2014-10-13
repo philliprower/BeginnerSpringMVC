37230ba4ffb8d99ddc2e89bcf82036f4cc9dc9d2
Phillip Rower <philliprower@gmail.com>
2014-10-13 13:50:26 -0700

    Update to add additional product URL with params.

--- src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
+++ src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.packt.webstore.domain.repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.packt.webstore.domain.Product;
 
@@ -9,4 +11,8 @@ public interface ProductRepository {
 	List <Product> getAllProducts();
 	
 	Product getProductById(String productID);
+	
+	List<Product> getProductsByCategory(String category);
+
+	Set<Product> getProductsByFilter(Map<String, List<String>> filterParams);
 }
--- src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
+++ src/main/java/com/packt/webstore/service/impl/ProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.packt.webstore.service.impl;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,25 @@ import com.packt.webstore.domain.repository.ProductRepository;
 import com.packt.webstore.service.ProductService;
 
 @Service
-public class ProductServiceImpl implements ProductService {
+public class ProductServiceImpl implements ProductService{
 	
 	@Autowired
-	ProductRepository productRepository;
+	private ProductRepository productRepository;
 
 	public List<Product> getAllProducts() {
 		return productRepository.getAllProducts();
+	}
+
+	public Product getProductById(String productID) {
+		return productRepository.getProductById(productID);
+	}
+	
+	public List<Product> getProductsByCategory(String category) {
+		return productRepository.getProductsByCategory(category);
+	}
+
+	public Set<Product> getProductsByFilter(Map<String, List<String>> filterParams) {
+		return productRepository.getProductsByFilter(filterParams);
 	}
 
 }
